9e2aa59db5f3a3781d4dd4517b5de92c255680c2
Bailey Kursar <bkursar@gmail.com>
2017-05-15 16:52:57 +0100

    Adding SequelizeJS and Postgres modules

--- product/index.js
+++ product/index.js
@@ -1,11 +1,1 @@
-var mapper = require("data-mapper-js");
-var pg = require("pg");
-
-var schemaBuilder = mapper.schema();
-    schemaBuilder
-    .add("users", "userid", "username", "useremail", "userpassword")
-        .hasManyWithMany("properties", "propertyid")
-        .save()
-    .add("properties", "propertyid", "propertyname", "propertydescription", "propertyprice").save();
-
-var db = mapper.init("pg", connString, schemaBuilder.schema);
+// Adding the Sequelize module in order to create tables in our database
