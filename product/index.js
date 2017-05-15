8b45209d0842b98065cfd2dd792622cd0eb842de
Bailey Kursar <bkursar@gmail.com>
2017-05-15 17:46:18 +0100

    Started off with Sequelize

--- product/index.js
+++ product/index.js
@@ -1,1 +1,33 @@
 // Adding the Sequelize module in order to create tables in our database
+
+var Sequelize = require('sequelize');
+
+var connection = new Sequelize('makersbnb', 'baileykursar', 'elephant', {
+  dialect: 'postgres'
+});
+
+var User = connection.define('user', {
+  name:     Sequelize.STRING,
+  email:    Sequelize.STRING,
+  password: Sequelize.STRING
+});
+
+// connection.sync().then(function () {
+//   User.create({
+//     name:     'Bailey',
+//     email:    'fake@email.com',
+//     password: 'password',
+//   });
+// });
+
+// connection.sync().then(function () {
+//   User.findById(1).then(function(user) {
+//     console.log(user.dataValues);
+//   })
+// })
+
+connection.sync().then(function () {
+  User.findAll().then(function(users) {
+    console.log(users.length);
+  })
+})
