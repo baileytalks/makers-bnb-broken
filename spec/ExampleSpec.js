1d78492bcb84ec057c274d57a02ca4adc283bc1c
bruxelles86 <w_gant@yahoo.co.uk>
2017-05-16 10:10:50 +0100

    Adds jasmine with example spec

---
+++ spec/ExampleSpec.js
@@ -0,0 +1,5 @@
+describe("example", function() {
+  it("returns 3", function() {
+    expect(example()).toEqual(3);
+  });
+});
---
+++ src/Example.js
@@ -0,0 +1,2 @@
+example = function() { var answer = 1 + 2
+return answer }
