a3c5a048ca78f75f5490a4d5f5d0446bbed40617
Bailey Kursar <bkursar@gmail.com>
2017-05-17 11:21:25 +0100

    Changing our repo across to Tim's express framework etc.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+// Set up the express app
+//const app = express();
+var app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//----------------------------------------------------------------
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.render("home",{Error:"none"});
+});
+
+
+
+
+//----------------------------------------------------------------
+
+module.exports = app;
--- src/Example.js
+++
@@ -1,2 +0,0 @@
-example = function() { var answer = 1 + 2
-return answer }
